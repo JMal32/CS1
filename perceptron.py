5e730c39c98af4f8f5c763359ab2304c013821dd
Justin M <justin.malone32@gmail.com>
2024-12-02 22:50:32 -0600

    fixing stuff in perceptron.py

--- perceptron.py
+++ perceptron.py
@@ -1,4 +1,4 @@
-import pandas as pd
+
 
 class perceptron:
     def __init__(self, x, y):
@@ -13,13 +13,14 @@ class perceptron:
         with open(filename, 'r') as f:
             for line in f:
                 data = line.strip().split(',')
-                self.x.append(float(values[0]))
-                self.y.append(float(values[1]))
-                self.truth.append(int(values[2]))
+                self.x.append(float(data[0]))
+                self.y.append(float(data[1]))
+                self.truth.append(int(data[2]))
 
     def trainPerceptron(self):
         numCorrect = 0
         dataset_size = len(self.x)
+        
         while numCorrect < dataset_size:
             numCorrect = 0
             for xi, yi, truth in zip(self.x, self.y, self.truth):
@@ -27,7 +28,7 @@ class perceptron:
 
                 if (mdl > 0 and truth > 0) or (mdl < 0 and truth < 0):
                     numCorrect += 1
-                elif: mdl * truth < 0:
+                elif mdl * truth < 0:
                     self.c1 += 1
                     self.c2 += xi
                     self.c3 += yi
@@ -36,4 +37,5 @@ class perceptron:
                     self.c2 -= xi
                     self.c3 -= yi
 
-                print(f"Updated coefficients: c1
+                print(f"Updated coefficients: {self.c1}, {self.c2}, {self.c3}")
+                
